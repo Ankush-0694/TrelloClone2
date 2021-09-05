031bf0729a60caa1d2ed2a66660bb15e220ed851
Ankush Kumar <ankushkashyap0000@gmail.com>
2021-09-05 14:29:44 +0530

    prepare to deploy

--- server.js
+++ server.js
@@ -25,12 +25,15 @@ app.use(cors());
 app.use("/auth", require("./src/auth/api"));
 app.use("/card", require("./src/cardList/api"));
 
-// server static assets in production
-app.use(express.static(path.join(__dirname, "../build")));
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../build"));
-});
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 5001;
 
